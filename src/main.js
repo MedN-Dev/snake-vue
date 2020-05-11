2f413d3cbd6c4d6d0a0e388259b8cb0eea52f0a1
Med Nabouli <med.nabouli@tracktik.com>
2020-05-11 16:17:17 -0400

    Snake Game

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import Resource from "vue-resource";
+import VueRouter from "vue-router";
+import App from "./App";
 import router from "./router";
 import store from "./store";
+// Importing the global css file
+import "@/assets/global.scss";
 
 Vue.config.productionTip = false;
+Vue.use(Resource);
+Vue.use(VueRouter);
+Vue.config.debug = true;
 
 new Vue({
   router,
